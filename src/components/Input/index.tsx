d5d946639b89b2c3904f1ca58e2139e26547aea4
“Brenno <dev.brennoeudes@gmail.com>
2024-09-05 20:41:51 -0300

    extends

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,18 @@
-import './styles.css';
-import { Spin } from '../Spin';
+import "./styles.css";
+import { Spin } from "../Spin";
+import { InputHTMLAttributes } from "react";
 
-interface Props {
+// EXTENDS: é uma forma de estender a interface, herdando propriedades de um elemento HTML. No exemplo, estamos estendendo as propriedades do input HTML abaixo. Assim também poderemos passar todas as props na fcn!
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   isLoading?: boolean; // ? significa que a propriedade é opcional.
 }
 
 export function Input({ isLoading = false, ...rest }: Props) {
   return (
-    <div className="input" >
-      <input type='text' {...rest} />
+    <div className="input">
+      <input type="text" {...rest} />
 
       {isLoading && <Spin />}
     </div>
-  )
+  );
 }
